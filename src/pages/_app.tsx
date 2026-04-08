7a7c97b0f8008febf1fd5a0b0ed6a2513aa277f7
Alex Shick <business@alexshick.com>
2026-04-08 10:45:41 -0400

    Added a state management file for the preloader animation.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import { Preloader } from "@/components/Preloader";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <Preloader isLoading={isLoading} />
+      <Component {...pageProps} />
+    </>
+  );
+}
