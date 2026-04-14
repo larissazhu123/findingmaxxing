f4bb21f1cf5473806f1aaead5f0252ff898e29d3
Alex Shick <business@alexshick.com>
2026-04-13 22:31:06 -0400

    Removed the frontend directory.

--- frontend/src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
-// If you have generated DB types, import them and use SupabaseClient<Database>
-/* import type { Database } from "./database.types"; */
-
-export const supabase: SupabaseClient /* <Database> */ = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
